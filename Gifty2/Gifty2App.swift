5f4df81cf5a288cc1518f7366c8dbf59ad4155e0
Matt <bryantm1122@gmail.com>
2023-04-24 15:28:01 +0200

    Initial Commit

---
+++ Gifty2/Gifty2App.swift
@@ -0,0 +1,17 @@
+//
+//  Gifty2App.swift
+//  Gifty2
+//
+//  Created by Matt Bryant on 4/24/23.
+//
+
+import SwiftUI
+
+@main
+struct Gifty2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
